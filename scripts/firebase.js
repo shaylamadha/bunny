b7f6679d1138f1677d1d08742be6c6612daf0f74
Jonathan <theshagod@gmail.com>
2021-04-16 10:20:40 -0700

    f

---
+++ scripts/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyApFEL8cCGnwhf3XfuLnP11kQiDj9S7IQg",
+    authDomain: "bunny-e19f3.firebaseapp.com",
+    projectId: "bunny-e19f3",
+    storageBucket: "bunny-e19f3.appspot.com",
+    messagingSenderId: "721457363676",
+    appId: "1:721457363676:web:4c6a705688d521d4439219",
+    measurementId: "G-3050MR5VGH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
