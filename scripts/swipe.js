b3adc6911bf679c8171b9b5fc21c1f609fb7f4ac
Jonathan <theshagod@gmail.com>
2021-04-30 22:05:11 -0700

    changing window

--- scripts/swipe.js
+++ scripts/swipe.js
@@ -1,6 +1,10 @@
-onAuthChange().then(user => {
-    if (!user) {
+let user = null;
+onAuthChange().then(user1 => {
+    if (!user1) {
       location.replace('./login.html');
+    } else {
+        user = user1;
+        console.log(user.email)
     }
   })
 
@@ -18,13 +22,34 @@ var p5 = document.getElementById("person5");
 var name_text = document.getElementById("name");
 var comment_text = document.getElementById("comment");
 
-function nextUser(users) { 
+async function nextUser(users) { 
     let copyOfUsers = [...users];
     let nextUser = copyOfUsers.shift();
-    
+    console.log(nextUser);
+    profile_img.src = nextUser.pic;
+    name_text.textContent = nextUser.name;
+    comment_text.textContent = nextUser.bio + "email: " + nextUser.email;
     return copyOfUsers;
 }
 
+let users = [];
+db.collection("users").get().then(async snap => {
+    users = await snap.docs.map(doc => doc.data());
+    console.log(users)
+    nextUser(users)
+    //get users
+    
+    // profile_img.src = "./images/justin.jpg"
+    // // p1.src = "./images/lion.jpg"
+    // // p2.src = "./images/lion.jpg"
+    // // p3.src = "./images/lion.jpg"
+    // // p4.src = "./images/lion.jpg"
+    // // p5.src = "./images/lion.jpg"
+
+    // name_text.textContent = "Justin"
+    // comment_text.textContent = "What's my ..."
+
+})
 
 
 document.addEventListener("click", e => {
@@ -35,22 +60,4 @@ document.addEventListener("click", e => {
     } else if (e.target === swipeBtns[1]) {
         console.log("Like clicked")
     }
-    console.log(document.getElementsByClassName("texts"))
-    profile_img.src = "./images/justin.jpg"
-    // p1.src = "./images/lion.jpg"
-    // p2.src = "./images/lion.jpg"
-    // p3.src = "./images/lion.jpg"
-    // p4.src = "./images/lion.jpg"
-    // p5.src = "./images/lion.jpg"
-
-    name_text.textContent = "Justin"
-    comment_text.textContent = "What's my ..."
-
-})
-
-//get users
-db.collection("users").get().then(querySnapShot => {
-    querySnapShot.forEach(doc => {
-        //console.log(doc.data());
-    })
 })
